992eab6b41f742a9a694aa7ff917372013633aff
Erik <miller.erik.1998@gmail.com>
2023-04-23 01:28:16 +0100

    Auth

--- app/src/main/java/com/example/splitexpenses/Login.kt
+++ app/src/main/java/com/example/splitexpenses/Login.kt
@@ -5,29 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
 
 class Login : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_login)
-//
-//
-//    }
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: AppCompatButton
     private lateinit var registerText: TextView
-//    private lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-//        auth = Firebase.auth
-//
+        auth = Firebase.auth
+
         emailEditText = findViewById(R.id.email)
         passwordEditText = findViewById(R.id.password)
         loginButton = findViewById(R.id.login_button)
@@ -37,32 +35,25 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
-//
-//        loginButton.setOnClickListener {
-//            val email = emailEditText.text.toString()
-//            val password = passwordEditText.text.toString()
-//            loginUser(email, password)
-//        }
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            loginUser(email, password)
+        }
     }
 
-//    private fun loginUser(email: String, password: String){
-//        auth.signInWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//
-//                    val user = auth.currentUser
-//                    val intent = Intent(
-//                        this,
-//                        DashboardActivity::class.java
-//                    )
-//
-//                    startActivity(intent)
-//                } else {
-//
-//                    Toast.makeText(baseContext, "Authentication failed.",
-//                        Toast.LENGTH_SHORT).show()
-//
-//                }
-//            }
-//    }
+    private fun loginUser(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val user = auth.currentUser
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.",Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
--- app/src/main/java/com/example/splitexpenses/Register.kt
+++ app/src/main/java/com/example/splitexpenses/Register.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Register : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_register)
-//    }
 
-//    private lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
@@ -24,7 +24,7 @@ class Register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-//        auth = Firebase.auth
+        auth = Firebase.auth
 
         emailEditText = findViewById(R.id.email)
         passwordEditText = findViewById(R.id.password)
@@ -37,30 +37,25 @@ class Register : AppCompatActivity() {
         }
 
 
-//        registerButton.setOnClickListener {
-//            val email = emailEditText.text.toString()
-//            val password = passwordEditText.text.toString()
-//            registerUser(email, password)
-//        }
+        registerButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            registerUser(email, password)
+        }
     }
 
-//    private fun registerUser(email: String, password: String) {
-//        auth.createUserWithEmailAndPassword(email, password)
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                   
-//                    val user = auth.currentUser
-//                    val intent = Intent(this, DashboardActivity::class.java)
-//                    startActivity(intent)
-//
-//                } else {
-//
-//                    Toast.makeText(
-//                        baseContext, "Authentication failed.",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//
-//                }
-//            }
-//    }
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val user = auth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
