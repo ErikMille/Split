249148934dc2f8aa1800d9804572e51c006aa6fa
Erik <miller.erik.1998@gmail.com>
2023-04-27 01:47:17 +0100

    Removed refresh

--- app/src/main/java/com/example/splitexpenses/GroupActivity.kt
+++ app/src/main/java/com/example/splitexpenses/GroupActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class GroupActivity : AppCompatActivity() {
     private var list: ArrayList<FriendModel>? = arrayListOf<FriendModel>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group)
